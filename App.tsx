95a8da4fd23005002c4eb8484b511a1618a4796d
이준환 <niip00ng@daum.net>
2020-04-10 11:12:37 +0900

    [DEV] 화면전환 슬라이드

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {
+    createStackNavigator,
+    TransitionPresets,
+  } from '@react-navigation/stack';
 import MainPage from './src/view/MainPage';
 import MyLotto from './src/view/MyLotto';
 import Welcome from './src/view/WelcomeUser';
@@ -9,18 +12,24 @@ import InputWord from './src/view/InputWord';
 import GenerateNumber from './src/view/GenerateNumber';
 import NumberResult from './src/view/NumberResult';
 
-const Stack = createStackNavigator();
+const baseTransitionOption = {
+    ...TransitionPresets.SlideFromRightIOS
+}
+const myLottoTransitionOption = {
+    ...TransitionPresets.ModalSlideFromBottomIOS
+}
 
+const Stack = createStackNavigator();
 const App = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator headerMode="none" initialRouteName="MainPage">
-                <Stack.Screen name="MainPage" component={MainPage} />
-                <Stack.Screen name="Welcome" component={Welcome} />
-                <Stack.Screen name="InputWord" component={InputWord} />
-                <Stack.Screen name="GenerateNumber" component={GenerateNumber} />
-                <Stack.Screen name="NumberResult" component={NumberResult} />
-                <Stack.Screen name="MyLotto" component={MyLotto} />
+                <Stack.Screen name="MainPage" component={MainPage} options={baseTransitionOption}/>
+                <Stack.Screen name="Welcome" component={Welcome} options={baseTransitionOption}/>
+                <Stack.Screen name="InputWord" component={InputWord} options={baseTransitionOption}/>
+                <Stack.Screen name="GenerateNumber" component={GenerateNumber} options={baseTransitionOption}/>
+                <Stack.Screen name="NumberResult" component={NumberResult} options={baseTransitionOption}/>
+                <Stack.Screen name="MyLotto" component={MyLotto} options={myLottoTransitionOption}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
